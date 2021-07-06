b0caf5ad42a309fc59e5e540d0ad7409e14135fd
Prajesh Gupta <imprj.gupta@gmail.com>
2021-07-06 12:29:45 +0530

    updates

--- JIRA/script.js
+++ JIRA/script.js
@@ -2,18 +2,18 @@ let addbtn = document.querySelector(".add");
 let body = document.querySelector("body");
 let dltbtn = document.querySelector(".delete");
 
-addbtn.addEventListener("click", function(e){
+// addbtn.addEventListener("click", function(e){
 
-   let div = document.createElement("div");
+//    let div = document.createElement("div");
    
-   div.classList.add("modal");
+//    div.classList.add("modal");
 
-   body.append(div);
+//    body.append(div);
 
-});
+// });
 
 dltbtn = addEventListener("click", function(e){
 
-   
+    
 })
 
