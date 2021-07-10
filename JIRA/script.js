77df1341c448adda1677ad90cb7d4e86a924f38f
Prajesh Gupta <imprj.gupta@gmail.com>
2021-07-11 02:35:29 +0530

    updates

--- JIRA/script.js
+++ JIRA/script.js
@@ -8,7 +8,22 @@ addbtn.addEventListener("click", function(e){
    
    div.classList.add("modal");
 
-   div.innerHTML = '<div class="task-section"><div class="task-inner-container" contenteditable="true"></div></div><div class="modal-priority-section"><div class="priority-inner-container"><div class="modal-priority pink"></div><div class="modal-priority blue"></div><div class="modal-priority green"></div><div class="modal-priority black"></div></div></div>';
+   div.innerHTML = '<div class="task-section"><div class="task-inner-container" contenteditable="true"></div></div><div class="modal-priority-section"><div class="priority-inner-container"><div class="modal-priority pink"></div><div class="modal-priority blue"></div><div class="modal-priority green"></div><div class="modal-priority black selected"></div></div></div>';
+
+   let allModalPrority = div.querySelectorAll(".modal-priority");
+
+   for(let i=0;i<allModalPrority.length;i++){
+      allModalPrority[i].addEventListener("click", function(e){
+         
+         
+         for(let j=0;j<allModalPrority.length;j++){
+            allModalPrority[j].classList.remove("selected");
+         }
+
+         e.currentTarget.classList.add("selected");
+      });
+   }
+
 
    body.append(div); 
 
